d67bcdba941860babd3624f6365c11b573a314f8
Maia <maiataurus314@gmail.com>
2026-09-01 17:15:36 +0100

    add nodeinfo type

--- src/Meshtastic.d.ts
+++ src/Meshtastic.d.ts
@@ -1,5 +1,11 @@
 type MeshMessage = {
-    nodeID: string,
-    rxTimestamp: number,
-    message: string,
+    nodeID: string, // 9 chars
+    rxTimestamp: number, // unix timestamp
+    message: string, // up to 200 bytes
+}
+
+type NodeInfo = {
+    nodeID: string, // 9 chars
+    shortname: string, // up to 4 chars
+    longname: string // up to 36 chars
 }
