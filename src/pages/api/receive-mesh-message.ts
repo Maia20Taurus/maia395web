d5fcb8dfbc30cb8131feeb530bf71f7055025234
Maia <contact@maia395.dev>
2026-09-07 16:16:01 +0100

    allow endpoint to be called with offset parameter

--- src/pages/api/receive-mesh-message.ts
+++ src/pages/api/receive-mesh-message.ts
@@ -3,9 +3,11 @@ export const prerender = false
 import type { APIRoute } from "astro";
 import { env } from 'cloudflare:workers';
 
-export const GET = (async ({ request }) => {
+export const GET = (async ({ params, request }) => {
+  const offset = Number(params.offset) || 0;
+
   const stub = env.MESHCHAT_SERVER_DO.getByName("foo");
-  const messageRows = await stub.get_last_messages_with_offset(100, 0);
+  const messageRows = await stub.get_last_messages_with_offset(100, offset);
 
   return new Response(JSON.stringify(messageRows), {status:200});
 }) satisfies APIRoute;
