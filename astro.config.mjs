f1653189db06506c2f95d04e5676d258c84ebf63
Maia <maiataurus314@gmail.com>
2026-09-10 00:40:32 +0100

    change to static mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({ imageService: "compile" }),
-  output: "server",
+  output: "static",
   session: false,
 });
